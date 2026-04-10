41f4e9fcfab7df0d094b3b9734be275d247ea03b
Johnnydeeps <jack.aldus@gmail.com>
2026-04-10 20:48:10 +0100

    L: added destruction/ collision check with a nested loop inside the existing collision check for asteroids and the player. correctly removes the shot object and asteroid object when colliding.

--- main.py
+++ main.py
@@ -61,6 +61,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         for instance in drawable:
             instance.draw(screen)
         pygame.display.flip()
