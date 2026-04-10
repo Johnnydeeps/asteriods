242005b1cbd273ed2840a2d643435addcee8a828
Johnnydeeps <jack.aldus@gmail.com>
2026-04-10 21:17:46 +0100

    M: added split method function to asteroid.py. correctly creating two random, smaller asteroids from one larger asteroid when hit with shot object via collission method function.

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    # new split method function to split and reduces size of asteroids hit by a shot.
+    # Reducing from Large, Medium, Small. when Small (ASTEROID_MIN_RADIUS) is hit,
+    # delete from the screen.
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        a_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        b_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+
+        a_asteroid.velocity = a * 1.2
+        b_asteroid.velocity = b * 1.2
